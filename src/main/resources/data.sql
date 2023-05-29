98c37bf4636d81b3e7bd638d03bd5a2300115f35
Christopher King <Christopher.king@orange.com>
2023-05-29 15:42:22 +0300

    Adding teachers in the teacher table

---
+++ src/main/resources/data.sql
@@ -0,0 +1,23 @@
+insert into student (ID,NAME,STUDY_YEAR)
+values (1001,'Michael',3);
+
+insert into student (ID,NAME,STUDY_YEAR)
+values (1002,'George',3);
+
+insert into student (ID,NAME,STUDY_YEAR)
+values (1003,'David',3);
+
+insert into student (ID,NAME,STUDY_YEAR)
+values (1004,'Jane',3);
+
+insert into teacher (ID,NAME,COURSE)
+values (10,'Patrick','Ethics');
+
+insert into teacher (ID,NAME,COURSE)
+values (11,'Jean','Philosophy');
+
+insert into teacher (ID,NAME,COURSE)
+values (12,'Eric','Programming');
+
+insert into teacher (ID,NAME,COURSE)
+values (13,'Maria','Arts');
